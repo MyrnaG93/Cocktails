6e04d999702fd14bc3edcca2e2b8079328e94639
Myrna <myrnagomes@hotmail.com>
2022-01-24 01:25:59 +0000

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        In incididunt cillum Lorem officia sint mollit minim Lorem. Culpa quis ut in qui ea cupidatat consequat. Do nisi culpa ullamco incididunt occaecat irure consequat deserunt nisi incididunt Lorem veniam. Velit amet do proident non cillum pariatur eu. Laborum quis reprehenderit nisi nulla nulla est amet est commodo. Amet et excepteur ullamco culpa pariatur ad. Laboris id aliqua ut pariatur.
+      </p>
+    </section>
+  )
+}
+
+export default About
